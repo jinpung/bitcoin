7ee80eb07e873e32deb531a529b6c0a488bc7c63
jinpung <jinpung@hotmail.com>
2017-12-06 06:48:27 +0200

    fixed BTG

--- src/app/dashboard/dashboard.ctrl.js
+++ src/app/dashboard/dashboard.ctrl.js
@@ -421,6 +421,12 @@ angular.module('inspinia').controller('dashboardCtrl', function(financeSvc, $int
                       stOMGBuy = valueBuy;
                       stOMGSell = valueSell;
                       break;
+                  case 'BTG':
+                      stBTG = value;
+                      stBTGBuy = valueBuy;
+                      stBTGSell = valueSell;
+                      break;
+                      
                 default:
                   break;
               }
@@ -501,6 +507,12 @@ angular.module('inspinia').controller('dashboardCtrl', function(financeSvc, $int
                     vm.viewArray[siteIndex].data[vmIndex].realVMRateSend = chVMRateBuy - stOMGSell
                     vm.viewArray[siteIndex].data[vmIndex].realVMRateRecv = stOMGBuy - chVMRateSell;
                     break;
+                 case 'BTG':
+                    vm.viewArray[siteIndex].data[vmIndex].realVMRate = stBTG - chVMRate;
+                    vm.viewArray[siteIndex].data[vmIndex].realVMRateSend = chVMRateBuy - stBTGSell
+                    vm.viewArray[siteIndex].data[vmIndex].realVMRateRecv = stBTGBuy - chVMRateSell;
+                    break;
+
                   default:
                   break;
               }
